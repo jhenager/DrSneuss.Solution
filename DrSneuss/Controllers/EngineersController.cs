7b2061a751f0a5f47edf8ecb69de956e55ddea1f
James Henager <jameschenager@gmail.com>
2020-08-07 15:18:04 -0700

    add details route to engineers controller

--- DrSneuss/Controllers/EngineersController.cs
+++ DrSneuss/Controllers/EngineersController.cs
@@ -20,10 +20,12 @@ namespace DrSneuss.Controllers
       List<Engineer> model = _db.Engineers.ToList();
       return View(model);
     }
+
     public ActionResult Create()
     {
       return View();
     }
+
     [HttpPost]
     public ActionResult Create(Engineer engineer, int MachineId)
     {
@@ -34,6 +36,15 @@ namespace DrSneuss.Controllers
       }
       _db.SaveChanges();
       return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisEngineer = _db.Engineers
+        .Include(engineer => engineer.Machines)
+        .ThenInclude(join => join.Machine)
+        .FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
     }
   }
 }
