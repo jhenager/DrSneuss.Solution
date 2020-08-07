54b3f1448c33f347265bf172c8db8ccc15cf0ac1
James Henager <jameschenager@gmail.com>
2020-08-07 15:13:59 -0700

    add create view to engineers

--- DrSneuss/Controllers/EngineersController.cs
+++ DrSneuss/Controllers/EngineersController.cs
@@ -20,5 +20,20 @@ namespace DrSneuss.Controllers
       List<Engineer> model = _db.Engineers.ToList();
       return View(model);
     }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Engineer engineer, int MachineId)
+    {
+      _db.Engineers.Add(engineer);
+      if (MachineId != 0)
+      {
+        _db.MachineEngineer.Add(new MachineEngineer() {MachineId = MachineId, EngineerId = engineer.EngineerId});
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
